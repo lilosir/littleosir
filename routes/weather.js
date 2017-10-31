2ad77471ddee32a67170295c937590c2aeace758
srygg <sryoliver@gmail.com>
2017-10-31 12:36:23 -0400

    add yahoo weather api

--- routes/weather.js
+++ routes/weather.js
@@ -2,11 +2,19 @@ var express = require('express');
 var router = express.Router();
 var apiai = require('apiai');
 var app = apiai("1439d8d1ff654084af8265fcfa6de923");
+var YQL = require('yql');
 
 /* post to weather. */
 router.post('/', function(req, res, next) {
-	console.log(req.body);
-	var text = req.body.text;
+	
+	var { text, place } = req.body;
+	var result = {
+		speech: "",
+		displayText: "",
+		data: {},
+		contextOut: [],
+		source: ""
+	};
 
   var dialogFlowRequest = app.textRequest(text, {
     sessionId: '123123123'
@@ -15,12 +23,30 @@ router.post('/', function(req, res, next) {
 	dialogFlowRequest.on('response', function(response) {
     console.log('response: ', response);
 
-	  res.send('weather request~~' + JSON.stringify(response));
+		var queryGeo = new YQL(`select * from geo.places where text="${place}"`);
+		queryGeo.exec(function (error, geolocation) {
+			// Do something with results (response.query.results)
+			// console.log(geolocation.query.results.place[0].woeid);
+			var woeid = geolocation.query.results.place[0].woeid;
+			var queryWeather = new YQL(`select item.condition from weather.forecast where woeid=${woeid} and u='c'`);
+			queryWeather.exec(function (error, weathers) {
+				console.log(weathers.query.results.channel.item.condition);
+				var { date, temp, text } = weathers.query.results.channel.item.condition;
+				result.speech = `In ${place}, it will be ${temp}, ${text}, ${date}`;
+				result.displayText = `In ${place}, it will be ${temp}, ${text}, ${date}`;
+				result.contextOut = [{"name":"weather", "lifespan":2, "parameters":{"geo-city":place, "date": date}}]
+				// woeid = geolocation.query.results.place[0].woeid;
+			});
+		});
+
+    res.set({'Content-type': 'application/json'});
+	  res.send(result);
 	});
 
 	dialogFlowRequest.on('error', function(error) {
 	    console.log('error: ', error);
-	    res.send('error: ' + error);
+	    res.status(500).send('error: ' + error);
+
 	});
 
 	dialogFlowRequest.end();
